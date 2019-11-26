8bc7401953ba2e5a9828748ccaa461646c44ede3
beefoo <brian@youaremyjoy.org>
2019-11-25 21:32:51 -0500

    implement header submenu

---
+++ js/ui.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var UI = (function() {
+
+  function UI(config) {
+    var defaults = {};
+    var globalConfig = typeof CONFIG !== 'undefined' ? CONFIG : {};
+    this.opt = _.extend({}, defaults, config, globalConfig);
+    this.init();
+  }
+
+  UI.prototype.init = function(){
+    this.loadListeners();
+  };
+
+  UI.prototype.loadListeners = function(){
+    $('.submenu-link').on('click', function(){
+      console.log($(this).attr('aria-expanded'))
+      var expanded = $(this).attr('aria-expanded') === 'true';
+      if (expanded) $(this).attr('aria-expanded', 'false');
+      else $(this).attr('aria-expanded', 'true');
+    });
+  };
+
+  return UI;
+
+})();
+
+$(function() {
+  var app = new UI({});
+});
